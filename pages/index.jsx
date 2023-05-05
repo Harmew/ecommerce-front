8795417cb6018682c36a549dcc8fcb9135bf5fe0
Daniel De Oliveira <daniel.oliveira@liggavc.com.br>
2023-05-05 14:31:00 -0300

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,30 @@
+import Featured from "@/components/Featured";
+import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import NewProducts from "@/components/NewProducts";
+
+export default function HomePage({ featuredProduct, newProducts }) {
+  return (
+    <div>
+      <Header />
+      <Featured product={featuredProduct} />
+      <NewProducts products={newProducts} />
+    </div>
+  );
+}
+
+export async function getServersideProps() {
+  const featuredProductId = "1";
+  await mongooseConnect();
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {
+    sort: { _id: -1 },
+    limit: 10,
+  });
+  return {
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
+  };
+}
